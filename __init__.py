af4cca6acb48635ae60f1a549825f7255b21bdc3
ukkeyHG <osaru3@saruyamax.com>
2026-02-21 09:04:39 +0900

    Fix node loading: use underscore in filename and standard import to comply with Python naming conventions

--- __init__.py
+++ __init__.py
@@ -1,14 +1,6 @@
 # RPG Monster Generator Node Package
-import importlib
 
-# モジュール名にハイフンが含まれるため、importlib を使用して動的にインポートします
-try:
-    node_module = importlib.import_module(".RPG-Monster-Generator", __package__)
-    NODE_CLASS_MAPPINGS = getattr(node_module, "NODE_CLASS_MAPPINGS")
-    NODE_DISPLAY_NAME_MAPPINGS = getattr(node_module, "NODE_DISPLAY_NAME_MAPPINGS")
-except ImportError:
-    # 互換性やエラーハンドリング
-    NODE_CLASS_MAPPINGS = {}
-    NODE_DISPLAY_NAME_MAPPINGS = {}
+from .rpg_monster_generator import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
